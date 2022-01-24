2cb4d71eab7a808f987ce410cd20b28c34404752
ibrkorkut07 <ibr.kor@gmail.com>
2022-01-24 06:51:41 +0100

    change

--- src/test/java/com/myfirstproject/Day01_FirstSeleniumClass.java
+++ src/test/java/com/myfirstproject/Day01_FirstSeleniumClass.java
@@ -3,7 +3,7 @@ package com.myfirstproject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstSeleniumClass {
+public class Day01_FirstSeleniumClass {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
